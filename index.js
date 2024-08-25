91c744f534e662cc7ae00cadbc542ef4fa4e8837
Rupesh Virani <rupeshvirani01@gmail.com>
2024-08-25 12:42:44 +0530

    resolved cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const userRoute = require('./routes/user')
 
 
@@ -9,6 +10,7 @@ mongoose.connect(process.env.MONGO_URI).
 then(()=> console.log("MongoDB Connected"))
 .catch((err)=> console.log("MongoDB connection error:", err));
 
+app.use(cors());
 app.use(express.json());
 app.use('/auth',userRoute);
 
